9803940bf206c3ce852530a5caeffede7a07b145
luguin444 <gustavo.ganimi@engenharia.ufjf.br>
2021-02-01 19:03:03 -0300

    feat: Initial Structure

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react'
+//import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+
+
+export default function App () {
+
+    
+    return (
+        <h1>Teste</h1>
+    );
+}
