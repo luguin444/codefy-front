e7d88884a5207ac6f2ce50942d3b76f2c46f2cec
brunatb <brutopanb@gmail.com>
2021-02-08 17:14:11 -0300

    fix: showing only the first name at home banner

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,12 +5,12 @@ import StyledHome from './styles';
 export default function Home(){
     // eslint-disable-next-line no-unused-vars
     const [activeCourses, setActiveCourser] = useState([]);
-    const name = localStorage.getItem('name');
+    const firstName = localStorage.getItem('name').split(' ')[0];
 
     return (
       <StyledHome>
         <div className="banner">
-          <p>Olá, {name}!</p>
+          <p>Olá, <span className="name">{firstName}</span>!</p>
           <b>{activeCourses.length === 0 ? 'Você não começou nenhum curso ainda. Experimente um! :)' : 'Continue seu curso atual abaixo! :)'}</b>
         </div>
         <SuggestedCourses />
