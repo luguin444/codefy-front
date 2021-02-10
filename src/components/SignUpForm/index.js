ddba37fd4bc0f16d4a5bbbe6384ab2e87f67eb8b
brunatb <brutopanb@gmail.com>
2021-02-10 16:38:14 -0300

    fix: registration data persists and cleaning login fields

---
+++ src/components/SignUpForm/index.js
@@ -0,0 +1,80 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+
+import Form from '../Form';
+import Button from '../Button';
+
+export default function SignUpForm (props) {
+    const { 
+        email,
+        setEmail,
+        password,
+        setPassword,
+        name,
+        setName,
+        confirmPassword,
+        setConfirmPassword,
+        setRegistered,
+     } = props;
+    const [loading, setLoading] = useState(false);
+
+    function signUpRoute(event) {
+        event.preventDefault();
+        setLoading(true);
+        const bodyRequest = { name, email, password, confirmPassword };
+        axios.post(`${process.env.API_BASE_URL}/clients/signup`, bodyRequest).then(({ data }) => {
+            setLoading(false);
+            setRegistered(true);
+            setEmail('');
+            setPassword('');
+            setName('');
+            setConfirmPassword('');
+            console.log(data);
+        }).catch((err) => {
+            if (err.response.data.error === 'Senhas diferentes.') {
+                alert('As senhas não são iguais. Tente novamente');
+            } else if (err.response.status === 422) {
+                alert('Dados não estão no padrão. Nome e senha devem conter mais de 6 caracteres');
+            } else {
+                alert('Problema com o cadastro. Erro desconhecido');
+            }
+            setLoading(false);
+        });
+    }
+
+    return (
+      <Form onSubmit={signUpRoute}>
+        <input 
+            placeholder="nome completo"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+        />
+        <input
+            placeholder="e-mail"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            />
+        <input
+            placeholder="senha"
+            type="password"
+            value={password}
+            onChange={ (e) => setPassword(e.target.value)}
+            required
+        />
+        <input
+            placeholder="repetir senha"
+            type="password"
+            value={confirmPassword}
+            onChange={ (e) => setConfirmPassword(e.target.value)}
+            required
+        />
+        <Button disabled={loading}>
+          Cadastrar
+        </Button>
+      </Form>
+    );
+}
--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -1,16 +1,36 @@
 import React, { useState } from 'react';
 import LoginForm from '../../components/LoginForm.js';
+import SignUpForm from '../../components/SignUpForm/index.js';
 import Container from './styles';
 
 export default function LoginPage () {
-    const [registered, setRegistered] = useState(false);
+    const [registered, setRegistered] = useState(true);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
 
     return (
       <Container>
         <img src='assets/images/logo.png' alt="" />
         <h2> learn. practice. code.</h2>
         <div className='loginBox'>
-          <LoginForm registered={registered} setRegistered={setRegistered} />
+          {registered ?
+            <LoginForm />
+            :
+            <SignUpForm 
+            email={email}
+            setEmail={setEmail}
+            password={password}
+            setPassword={setPassword}
+            name={name}
+            setName={setName}
+            confirmPassword={confirmPassword}
+            setConfirmPassword={setConfirmPassword}
+            setRegistered={setRegistered}
+            />
+          }
+          
           <button onClick={() => setRegistered(!registered)} className="footer"> 
             { registered ?
                         'Primeira vez ? Crie uma conta!' :
