286306fe1a90d0fd26fe0dea8e5953e38b154981
luguin444 <gustavo.ganimi@engenharia.ufjf.br>
2021-02-24 15:56:51 -0300

    feat: layout dropDown sign out and profile

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import Avatar from 'react-avatar';
@@ -6,8 +6,10 @@ import StyledHeader from './styles';
 import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io';
 
 export default function Header(){
+  const [dropDownisClosed, setDropDownisClosed] = useState(true);
   const currentRoute = useLocation();
   const name = localStorage.getItem('name');
+  
 
   if (currentRoute.pathname === '/' || currentRoute.pathname.includes('/courses')) {
     return null;
@@ -29,6 +31,17 @@ export default function Header(){
         <li className="courses">Cursos</li>
       </ul>
       <div className='user'>
+        {
+          dropDownisClosed ?  
+            <IoIosArrowDown className="icon" onClick={() => setDropDownisClosed(!dropDownisClosed)}/> : 
+            <>
+              <ul className="dropDown">
+                <li> Perfil </li>
+                <li> Sair </li>
+              </ul>
+              <IoIosArrowUp className="icon" onClick={() => setDropDownisClosed(!dropDownisClosed)}/>
+            </>
+        }
         <Avatar name={name} round={true} size="4em" maxInitials={2}/>
       </div>
     </StyledHeader>
